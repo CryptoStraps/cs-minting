f8bbc6376cef2f2cc4f97d5ed85c96a48f3e46da
Myron Ramirez <radmramirez@gmail.com>
2022-03-06 19:14:01 -0600

    moved mint

--- src/Home.tsx
+++ src/Home.tsx
@@ -281,7 +281,7 @@ const Home = (props: HomeProps) => {
         />
         {(candyMachine?.state?.goLiveDate?.toNumber() || 0) * 1000 <=
           Date.now() && (
-          <div className="CrossMint">
+            <div className="CrossMint">
             <CrossMintButton
            
               collectionTitle="CryptoStraps"
@@ -314,6 +314,14 @@ const Home = (props: HomeProps) => {
                   <img alt="cs" src="/ltrans.png"></img>
               </div> 
               <ConnectButton className="CSbutton">Connect Wallet</ConnectButton>
+              <div className="CrossMint">
+            <CrossMintButton
+           
+              collectionTitle="CryptoStraps"
+              collectionDescription="CryptoStraps is a next-gen 3D animated NFT pushing all boundaries and breathing new life into the ecosystem with their innovate tech."
+              collectionPhoto="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/2Pyc8DkCgPohR1o3ExMx84fLxU8ti1eQnySUwJMh5E6d/logo.png"
+            />
+          </div>
           </>
             
           ) : (
